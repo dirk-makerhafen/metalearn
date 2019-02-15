acb260b8596a20ad230568673bc23b051c7c65f7
dirk-attraktor <dirk@mex21.net>
2019-02-15 20:29:16 +0000

    updates

--- client.py
+++ client.py
@@ -43,7 +43,7 @@ try:
             URL = 'http://%s' % URL
 
         if ARG == "daemon" and CNT == 0:
-            CNT = multiprocessing.cpu_count()/2
+            CNT = int(multiprocessing.cpu_count()/2)
 except:
     print('''
         client.py daemon url cores                  # run as daemon
